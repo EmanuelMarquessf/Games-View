6442eee7befb45261aac435a4f8779fcca4a8631
EmanuelMarquessf <emanuel.marquessf@gmail.com>
2025-01-23 10:45:00 -0300

    Resonsivity implement

--- src/components/NewsMainSection.jsx
+++ src/components/NewsMainSection.jsx
@@ -16,27 +16,25 @@ function NewsMainSection({ newsData }) {
       <h1 className="text-darkColdBlue-60 font-poppins text-3xl font-semibold capitalize">
         Last Games News
       </h1>
-      <div className="grid grid-row-2 grid-flow-col gap-8 aspect-[16/7] font-roboto ">
+      <div className="grid md:grid-rows-5 grid-flow-row lg:grid-rows-2   lg:grid-flow-col gap-8 md:aspect-[16/12] lg:aspect-[16/7] font-roboto ">
         {newsData.slice(0, 4).map((news, index) => (
           <a
             href={news.link}
             target="_blank"
             key={index}
-            className={`flex flex-col justify-end bg-cover bg-center rounded-xl opacity-75 hover:scale-105 hover:opacity-100 transition-all duration-300 ease-in-out ${
-              index === 0 ? "row-span-2 col-span-3" : ""
-            } ${index === 1 ? "col-span-2" : ""} ${
-              index >= 2 ? "row-start-2 col-span-1" : ""
-            }`}
+            className={`flex flex-col justify-end bg-cover bg-center rounded-xl opacity-75 hover:scale-105 hover:opacity-100 transition-all duration-300 ease-in-out aspect-[16/12] md:aspect-auto ${
+              index === 0 ? "row-span-1 md:row-span-3  md:col-span-3 " : index === 1 ? "col-span-1 lg:col-span-2 row-span-2 lg:row-span-1" : index >=2 ? "col-span-1 row-span-2 lg:row-start-2" : "" 
+            } `}
             style={{ backgroundImage: `url(${news.image_url})` }}
           >
-            <div className="flex flex-col gap-2 w-full rounded-b-lg px-4 py-2 bg-darkColdBlue-500 opacity-95">
+            <div className={`flex flex-col gap-2 justify-center w-full rounded-b-lg px-4 py-2 bg-darkColdBlue-500 opacity-95 `}>
               <h1
                 className={`w-full text-darkColdBlue-60 font-semibold line-clamp-2 font-poppins ${
                   index === 0
-                    ? "text-3xl"
+                    ? "text-lg md:text-3xl"
                     : index === 1
-                    ? "text-2xl"
-                    : "text-md"
+                    ? "text-lg lg:text-2xl"
+                    : "text-lg"
                 }`}
               >
                 {news.title}
@@ -55,17 +53,6 @@ function NewsMainSection({ newsData }) {
             </div>
           </a>
         ))}
-
-        {/* <div src={newsData[selectedNew].urlToImage} className="grid grid aspect-[16/9] bg-cover bg-center" style={{ backgroundImage: `url(${newsData[selectedNew].urlToImage})`}}> 
-
-</div>
-<span>{newsData[selectedNew].title}</span>
-
-{newsData.map((news, index) => (
-  <div onClick={() => setSelectedNew(index)}  style={{ backgroundImage: `url(${news.urlToImage})` }}>
-    <p>aa</p>
-  </div>
-))} */}
       </div>
     </div>
   );
